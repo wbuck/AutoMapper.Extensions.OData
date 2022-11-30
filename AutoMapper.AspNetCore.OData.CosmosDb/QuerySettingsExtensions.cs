6bced258c130ea98c50609008d342396b31e44a3
Warren <wbuckley15@hotmail.com>
2022-11-29 20:44:32 -0500

    add remaining public api

---
+++ AutoMapper.AspNetCore.OData.CosmosDb/QuerySettingsExtensions.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper.AspNet.OData;
+using System.Threading;
+
+namespace AutoMapper.AspNetCore.OData.CosmosDb;
+internal static class QuerySettingsExtensions
+{
+    public static CancellationToken GetCancellationToken(this QuerySettings querySettings) =>
+        querySettings?.AsyncSettings?.CancellationToken ?? CancellationToken.None;
+}
--- AutoMapper.AspNetCore.OData.CosmosDb/QueryableExtensions.cs
+++ AutoMapper.AspNetCore.OData.CosmosDb/QueryableExtensions.cs
@@ -4,6 +4,7 @@ using LogicBuilder.Expressions.Utils.Expansions;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.Azure.Cosmos.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -15,7 +16,25 @@ namespace AutoMapper.AspNetCore.OData.CosmosDb;
 
 public static class QueryableExtensions
 {
-    //public static async Task<ICollection<TModel>> GetAsync<>
+    public static ICollection<TModel> Get<TModel, TData>(this IQueryable<TData> query,
+        IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings = null)
+         where TModel : class
+    {
+        IQueryable<TModel> modelQuery = query.GetQuery(mapper, options, querySettings);
+        return modelQuery.ToArray();
+    }
+
+    public static async Task<ICollection<TModel>> GetAsync<TModel, TData>(this IQueryable<TData> query, 
+        IMapper mapper, ODataQueryOptions<TModel> options, QuerySettings querySettings = null)
+            where TModel : class
+    {
+        IQueryable<TModel> modelQuery = await query
+            .GetQueryAsync(mapper, options, querySettings).ConfigureAwait(false);
+
+        return await modelQuery.ExecuteQueryAsync(querySettings.GetCancellationToken())
+            .ConfigureAwait(false);
+    }
+
     public static async Task<IQueryable<TModel>> GetQueryAsync<TModel, TData>(
         this IQueryable<TData> query, 
         IMapper mapper, 
@@ -26,25 +45,28 @@ public static class QueryableExtensions
             querySettings?.ODataSettings?.HandleNullPropagation ?? HandleNullPropagationOption.False,
             querySettings?.ODataSettings?.TimeZone);
 
-        await query.ApplyOptionsAsync(mapper, filter, options, querySettings).ConfigureAwait(false);
+        ApplyOptions(options, querySettings);
+
+        await query.ApplyCountQueryAsync(mapper, filter, options, querySettings)
+            .ConfigureAwait(false);
+
         return query.GetQueryable(mapper, options, querySettings, filter);
     }
 
-    public static async Task ApplyOptionsAsync<TModel, TData>(
-        this IQueryable<TData> query, 
-        IMapper mapper, Expression<Func<TModel, bool>> filter, 
-        ODataQueryOptions<TModel> options, 
-        QuerySettings querySettings)
+    public static IQueryable<TModel> GetQuery<TModel, TData>(
+        this IQueryable<TData> query,
+        IMapper mapper,
+        ODataQueryOptions<TModel> options,
+        QuerySettings querySettings = null) where TModel : class
     {
+        Expression<Func<TModel, bool>> filter = options.ToFilterExpression(
+            querySettings?.ODataSettings?.HandleNullPropagation ?? HandleNullPropagationOption.False,
+            querySettings?.ODataSettings?.TimeZone);
+
         ApplyOptions(options, querySettings);
 
-        if (options.Count?.Value == true)
-        {
-            var cancellationToken = querySettings?.AsyncSettings?.CancellationToken ?? CancellationToken.None;
-        
-            options.AddCountOptionsResult(
-                await query.QueryCountAsync(mapper, filter, cancellationToken).ConfigureAwait(false));
-        }
+        query.ApplyCountQuery(mapper, filter, options);
+        return query.GetQueryable(mapper, options, querySettings, filter);
     }
 
     private static void ApplyOptions<TModel>(ODataQueryOptions<TModel> options, QuerySettings querySettings)
@@ -85,23 +107,63 @@ public static class QueryableExtensions
             ProjectionSettings projectionSettings = null)
     {
         Expression<Func<TData, bool>> f = mapper.MapExpression<Expression<Func<TData, bool>>>(filter);
-        Func<IQueryable<TData>, IQueryable<TData>> mappedQueryFunc = mapper.MapExpression<Expression<Func<IQueryable<TData>, IQueryable<TData>>>>(queryFunc)?.Compile();
+
+        Func<IQueryable<TData>, IQueryable<TData>> mappedQueryFunc = 
+            mapper.MapExpression<Expression<Func<IQueryable<TData>, IQueryable<TData>>>>(queryFunc)?.Compile();
 
         if (filter is not null)
             query = query.Where(f);
 
-        var includes = GetIncludes();
-
         return mappedQueryFunc is not null
-                ? mapper.ProjectTo(mappedQueryFunc(query), projectionSettings?.Parameters, includes)
-                : mapper.ProjectTo(query, projectionSettings?.Parameters, includes);
+                ? mapper.ProjectTo(mappedQueryFunc(query), projectionSettings?.Parameters, GetIncludes())
+                : mapper.ProjectTo(query, projectionSettings?.Parameters, GetIncludes());
 
         Expression<Func<TModel, object>>[] GetIncludes() => 
             includeProperties?.ToArray() ?? new Expression<Func<TModel, object>>[] { };
     }
 
-    private static async Task<int> QueryCountAsync<TModel, TData>(
-        this IQueryable<TData> query, 
+    public static void ApplyCountQuery<TModel, TData>(this IQueryable<TData> query,
+        IMapper mapper, Expression<Func<TModel, bool>> filter, ODataQueryOptions<TModel> options)
+    {
+        if (options.Count?.Value == true)
+        {
+            options.AddCountOptionsResult
+            (
+                query.QueryCount(mapper, filter)
+            );
+        }
+    }
+
+    private static int QueryCount<TModel, TData>(this IQueryable<TData> query, 
+        IMapper mapper, Expression<Func<TModel, bool>> filter)
+    {
+        if (filter is not null)
+        {
+            query = query.Where
+            (
+                mapper.MapExpression<Expression<Func<TData, bool>>>(filter)
+            );
+        }
+        return query.Count();
+    }
+
+    public static async Task ApplyCountQueryAsync<TModel, TData>(this IQueryable<TData> query,
+        IMapper mapper, 
+        Expression<Func<TModel, bool>> filter,
+        ODataQueryOptions<TModel> options,
+        QuerySettings querySettings)
+    {
+        if (options.Count?.Value == true)
+        {
+            options.AddCountOptionsResult
+            (
+                await query.QueryCountAsync(mapper, filter, querySettings.GetCancellationToken())
+                    .ConfigureAwait(false)
+            );
+        }
+    }
+
+    private static async Task<int> QueryCountAsync<TModel, TData>(this IQueryable<TData> query, 
         IMapper mapper, 
         Expression<Func<TModel, bool>> filter, 
         CancellationToken cancellationToken = default)
@@ -114,5 +176,12 @@ public static class QueryableExtensions
             );
         }                    
         return (await query.CountAsync(cancellationToken).ConfigureAwait(false)).Resource;
+    }
+
+    private static async Task<ICollection<TModel>> ExecuteQueryAsync<TModel>(
+        this IQueryable<TModel> query, CancellationToken cancellationToken = default)
+    {
+        using var iterator = query.ToFeedIterator();
+        return (await iterator.ReadNextAsync(cancellationToken).ConfigureAwait(false)).Resource.ToArray();
     }
 }
