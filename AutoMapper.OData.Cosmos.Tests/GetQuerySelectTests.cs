d27c73792f02f51793c85f0644245324ae221b16
Warren <wbuckley15@hotmail.com>
2022-12-27 13:47:32 -0500

    add tests

--- AutoMapper.OData.Cosmos.Tests/GetQuerySelectTests.cs
+++ AutoMapper.OData.Cosmos.Tests/GetQuerySelectTests.cs
@@ -5,11 +5,7 @@ using AutoMapper.OData.Cosmos.Tests.Mappings;
 using AutoMapper.OData.Cosmos.Tests.Models;
 using AutoMapper.OData.Cosmos.Tests.Persistence;
 using Microsoft.AspNetCore.OData;
-using Microsoft.AspNetCore.OData.Extensions;
 using Microsoft.AspNetCore.OData.Query;
-using Microsoft.OData.Edm;
-using Microsoft.OData.ModelBuilder;
-using Microsoft.OData.UriParser;
 using System.Reflection;
 
 namespace AutoMapper.OData.Cosmos.Tests;
@@ -517,95 +513,6 @@ public sealed class GetQuerySelectTests
         }
     }
 
-    [Fact]
-    public async Task ForestSelectValues_ShouldReturnLiteralCollectionOfValues()
-    {
-        const string query = "/forest?$select=Values($filter=$this gt 500 and $this lt 1000)";        
-        //const string query = "/forest?$select=DomainControllers($filter=$this/DcCredentials/Username eq 'administrator1')";
-        //const string query = "/forest?$expand=DomainControllers/Dc($expand=Backups($filter=ForestId eq 00000000-0000-0000-0000-000000000000))";
-        //const string query = "/forest?$select=Values($orderby=$this)";
-        //const string query = "/forest?$expand=DomainControllers/Dc($expand=Backups)";
-        Test(Get<ForestModel, Forest>(query));
-        Test(await GetAsync<ForestModel, Forest>(query));
-        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
-
-        static void Test(ICollection<ForestModel> collection)
-        {
-            Assert.Equal(3, collection.Count);
-            foreach (var model in collection)
-            {
-                AssertModel(model);
-            }
-        }
-
-        static void AssertModel(ForestModel model)
-        {
-            Assert.Equal(default, model.Id);
-            Assert.Equal(default, model.ForestId);
-            Assert.Null(model.ForestName);
-            Assert.Null(model.ForestWideCredentials);
-            Assert.Null(model.Metadata);
-            Assert.Empty(model.DomainControllers);
-            Assert.Equal(3, model.Values.Count);
-        }
-    }
-
-    [Fact]
-    public async Task TBD()
-    {
-        //const string query = "/forest?$select=ForestWideCredentials/Username, ForestWideCredentials/Password";
-        //const string query = "/forest?$select=ForestWideCredentials($select=Username), DomainControllers/Dc&$expand=DomainControllers/Dc($select=FullyQualifiedDomainName, Fake/FakeInternal/Age)";
-        //const string query = "/forest?$select=Fake($select=FakeInternal($select=Name, Age)), ForestName, DomainControllers($select=Dc, DateAdded, DcCredentials($select=Username, Password))&$expand=DomainControllers/Dc($select=Backups, FullyQualifiedDomainName;$expand=Backups($select=Location/Credentials/Username))";
-
-        const string query = "/forest?$select=Fake($select=FakeInternal($select=Name, Age)), ForestName, DomainControllers($select=Dc, DateAdded, DcCredentials)&$expand=DomainControllers/Dc($select=Backups, FullyQualifiedDomainName, Attributes, FsmoRoles;$expand=Backups($select=Location))";
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, ForestName, DomainControllers($select=Dc, DateAdded, DcCredentials)&$expand=DomainControllers/Dc($select=Backups, FullyQualifiedDomainName;$expand=Backups($select=Location))";
-
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, Fake/FakeInternal/Age, ForestName, DomainControllers/Dc, DomainControllers/DateAdded, DomainControllers/DcCredentials/Username, DomainControllers/DcCredentials/Password&expand=DomainControllers/Dc($select=Backups, FullyQualifiedDomainName;$expand=Backups($select=Location/Credentials/Username))";
-
-        // fake
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, Fake/FakeInternal/Age, ForestName, DomainControllers/Dc, DomainControllers/DateAdded, DomainControllers/DcCredentials/Username, DomainControllers/DcCredentials/Password&expand=DomainControllers/Dc($select=Complex/Complex2/Backups, Complex/Complex2/Username, FullyQualifiedDomainName;$expand=Complex/Complex2/Backups($select=Location/Credentials/Username))";
-
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, Fake/FakeInternal/Age, ForestName, Values($filter=$it lt 10)";
-        //const string query = "/forest?$select=DomainControllers($filter=DcCredentials/Username eq 'administrator')";
-        //const string query = "/forest?$select=ForestWideCredentials/Username, ForestWideCredentials/Password";
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, Fake/FakeInternal/Age, DomainControllers/Dc";
-
-        //const string query = "/forest?$expand=DomainControllers/Dc($select=Fake($select=FakeInternal($select=Name)), Backups;$expand=Backups)";
-        //const string query = "/forest?$expand=DomainControllers/Dc";
-
-        //const string query = "/forest?$select=Fake($select=FakeInternal($select=Name, Age))";
-        //const string query = "/forest?$select=Fake/FakeInternal/Name, Fake/FakeInternal/Age";
-
-        Test(Get<ForestModel, Forest>(query));
-        Test(await GetAsync<ForestModel, Forest>(query));
-        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
-
-        static void Test(ICollection<ForestModel> collection)
-        {
-            Assert.Equal(3, collection.Count);
-
-            foreach (var (model, forestName) in
-                collection.Zip(new[] { "Abernathy Forest", "Rolfson Forest", "Zulauf Forest" }))
-            {
-                AssertModel(model, forestName);
-            }
-        }
-
-        static void AssertModel(ForestModel model, string forestName)
-        {
-            Assert.NotEqual(default, model.ForestId);
-            Assert.NotEqual(default, model.Id);
-            Assert.NotEmpty(model.DomainControllers);
-            Assert.All(model.DomainControllers.Select(entry => entry.DcCredentials), creds => Assert.NotNull(creds));
-            Assert.All(model.DomainControllers.Select(entry => entry.DcNetworkInformation), loc => Assert.NotNull(loc));
-            Assert.All(model.DomainControllers.Select(entry => entry.Dc), dc => Assert.Null(dc));
-            Assert.Equal(forestName, model.ForestName);
-            Assert.NotNull(model.ForestWideCredentials);
-            Assert.NotNull(model.ForestWideCredentials.Username);
-            Assert.NotNull(model.ForestWideCredentials.Password);
-        }
-    }
-
     private Task<ICollection<TModel>> GetUsingCustomNameSpace<TModel, TData>(string query,
             ODataQueryOptions<TModel>? options = null, QuerySettings? querySettings = null) 
         where TModel : class 
@@ -674,53 +581,4 @@ public sealed class GetQuerySelectTests
             customNamespace
         );
     }
-}
-
-public static class ODataHelpers
-{
-    private const string BaseAddress = "http://localhost:16324";
-
-    public static ODataQueryOptions<T> GetODataQueryOptions<T>(string queryString, IServiceProvider serviceProvider, string? customNamespace = null) 
-        where T : class
-    {
-        var builder = new ODataConventionModelBuilder();
-
-        if (customNamespace is not null)
-            builder.Namespace = customNamespace;
-
-        builder.EntitySet<T>(typeof(T).Name);
-
-        IEdmModel model = builder.GetEdmModel();
-        IEdmEntitySet entitySet = model.EntityContainer.FindEntitySet(typeof(T).Name);
-        ODataPath path = new(new EntitySetSegment(entitySet));
-
-        var request = new DefaultHttpContext()
-        {
-            RequestServices = serviceProvider
-        }.Request;
-
-        //var oDataOptions = new ODataOptions().AddRouteComponents("key", model,
-        //    x => x.AddSingleton<ISearchBinder, OpsTenantSearchBinder>());
-        var oDataOptions = new ODataOptions().AddRouteComponents("key", model);
-        var (_, routeProvider) = oDataOptions.RouteComponents["key"];
-
-        request.ODataFeature().Services = routeProvider;
-        var oDataQueryOptions = new ODataQueryOptions<T>
-        (
-            new ODataQueryContext(model, typeof(T), path),
-            BuildRequest(request, new Uri(BaseAddress + queryString))
-        );
-        return oDataQueryOptions;
-
-        static HttpRequest BuildRequest(HttpRequest request, Uri uri)
-        {
-            request.Method = "GET";
-            request.Host = new HostString(uri.Host, uri.Port);
-            request.Path = uri.LocalPath;
-            request.QueryString = new QueryString(uri.Query);
-
-            return request;
-        }
-
-    }    
 }
--- AutoMapper.OData.Cosmos.Tests/GetQueryTests.cs
+++ AutoMapper.OData.Cosmos.Tests/GetQueryTests.cs
@@ -1,5 +1,139 @@
-﻿namespace AutoMapper.OData.Cosmos.Tests;
+﻿using AutoMapper.AspNet.OData;
+using AutoMapper.OData.Cosmos.Tests.Entities;
+using AutoMapper.OData.Cosmos.Tests.Infrastructure;
+using AutoMapper.OData.Cosmos.Tests.Mappings;
+using AutoMapper.OData.Cosmos.Tests.Models;
+using AutoMapper.OData.Cosmos.Tests.Persistence;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Query;
 
-public class GetQueryTests
+namespace AutoMapper.OData.Cosmos.Tests;
+
+[Collection(nameof(CosmosContainer))]
+public sealed class GetQueryTests
 {
+    private readonly CosmosContainer dbContainer;
+    private readonly IServiceProvider serviceProvider;
+
+    public GetQueryTests(CosmosContainer dbContainer)
+    {
+        this.dbContainer = dbContainer;
+
+        IServiceCollection services = new ServiceCollection();
+        IMvcCoreBuilder builder = new TestMvcCoreBuilder
+        {
+            Services = services
+        };
+
+        builder.AddOData();
+        services
+            .AddSingleton<IConfigurationProvider>(new MapperConfiguration(cfg => cfg.AddMaps(typeof(IAssemablyMarker).Assembly)))
+            .AddTransient<IMapper>(sp => new Mapper(sp.GetRequiredService<IConfigurationProvider>(), sp.GetService))
+            .AddTransient<IApplicationBuilder>(sp => new ApplicationBuilder(sp))
+            .AddRouting()
+            .AddLogging();
+
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    [Fact]
+    public async Task ForestSelectValues_NestedFilter_ShouldReturnFilteredLiteralCollectionOfValues()
+    {
+        const string query = "/forest?$select=Values($filter=$this gt 1 and $this lt 101)&$orderby=ForestName";
+        List<List<int>> expectedValues = new()
+        {
+            new() { 100 },
+            new() { 42, 2 },
+            new() { 2 },
+        };
+
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));       
+
+        void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(3, collection.Count);
+            foreach (var (model, expected) in collection.Zip(expectedValues))
+            {
+                AssertModel(model, expected);
+            }
+        }
+
+        static void AssertModel(ForestModel model, List<int> expected)
+        {
+            Assert.Equal(expected, model.Values);
+        }
+    }
+
+    private Task<ICollection<TModel>> GetUsingCustomNameSpace<TModel, TData>(string query,
+            ODataQueryOptions<TModel>? options = null, QuerySettings? querySettings = null)
+        where TModel : class
+        where TData : class
+    {
+        return GetAsync<TModel, TData>(query, options, querySettings, "com.FooBar");
+    }
+
+    private ICollection<TModel> Get<TModel, TData>(string query, ODataQueryOptions<TModel>? options = null)
+        where TModel : class
+        where TData : class
+    {
+        return
+        (
+            DoGet
+            (
+                this.dbContainer.GetContainer().GetItemLinqQueryable<TData>(allowSynchronousQueryExecution: true).AsQueryable(),
+                serviceProvider.GetRequiredService<IMapper>()
+            )
+        ).ToList();
+
+        ICollection<TModel> DoGet(IQueryable<TData> dataQueryable, IMapper mapper)
+        {
+            return dataQueryable.Get
+            (
+                mapper,
+                options ?? GetODataQueryOptions<TModel>(query),
+                new QuerySettings { ODataSettings = new ODataSettings { HandleNullPropagation = HandleNullPropagationOption.False } }
+            );
+        }
+    }
+
+
+    private async Task<ICollection<TModel>> GetAsync<TModel, TData>(
+        string query, ODataQueryOptions<TModel>? options = null, QuerySettings? querySettings = null, string? customNamespace = null)
+        where TModel : class
+        where TData : class
+    {
+        return
+        (
+            await DoGet
+            (
+                this.dbContainer.GetContainer().GetItemLinqQueryable<TData>().AsQueryable(),
+                serviceProvider.GetRequiredService<IMapper>()
+            )
+        ).ToList();
+
+        async Task<ICollection<TModel>> DoGet(IQueryable<TData> dataQueryable, IMapper mapper)
+        {
+            return await dataQueryable.GetAsync
+            (
+                mapper,
+                options ?? GetODataQueryOptions<TModel>(query, customNamespace),
+                querySettings!
+            );
+        }
+    }
+
+    private ODataQueryOptions<TModel> GetODataQueryOptions<TModel>(string query, string? customNamespace = null)
+        where TModel : class
+    {
+        return ODataHelpers.GetODataQueryOptions<TModel>
+        (
+            query,
+            serviceProvider,
+            customNamespace
+        );
+    }
+
+
 }
