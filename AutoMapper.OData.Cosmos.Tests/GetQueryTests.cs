73a53bc6a5eb6f2db660871ddba6d10cbf795994
Warren <wbuckley15@hotmail.com>
2022-12-28 20:50:11 -0500

    add more tests

--- AutoMapper.OData.Cosmos.Tests/GetQueryTests.cs
+++ AutoMapper.OData.Cosmos.Tests/GetQueryTests.cs
@@ -122,9 +122,9 @@ public sealed class GetQueryTests
     }
 
     [Fact]
-    public async void ForestModelExpandBuildingsFilterEqAndOrderBy()
+    public async void ForestModelExpandDcFilterEqAndOrderBy()
     {
-        string query = "/ForestModel?$top=5&$expand=DomainControllers/Dc&$filter=ForestName eq 'Rolfson Forest'&$orderby=ForestName desc";
+        string query = "/forest?$top=5&$expand=DomainControllers/Dc&$filter=ForestName eq 'Rolfson Forest'&$orderby=ForestName desc";
         Test(Get<ForestModel, Forest>(query));
         Test(await GetAsync<ForestModel, Forest>(query));
         Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
@@ -137,6 +137,175 @@ public sealed class GetQueryTests
             Assert.Equal("Rolfson Forest", collection.First().ForestName);
         }
     }
+
+    [Fact]
+    public async void ForestModelExpandDcFilterNeAndOrderBy()
+    {
+        const string query = "/forest?$top=5&$expand=DomainControllers/Dc&$filter=ForestName ne 'Zulauf Forest'&$orderby=ForestName desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(2, collection.Count);
+            Assert.Equal(4, collection.First().DomainControllers.Count);
+            Assert.Equal(4, collection.Last().DomainControllers.Count);
+            Assert.All(collection.First().DomainControllers.Select(m => m.Dc), dc => Assert.NotNull(dc));
+            Assert.All(collection.Last().DomainControllers.Select(m => m.Dc), dc => Assert.NotNull(dc));
+            Assert.Equal("Rolfson Forest", collection.First().ForestName);
+            Assert.Equal("Abernathy Forest", collection.Last().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelFilterEqNoExpand()
+    {
+        const string query = "/forest?$filter=ForestName eq 'Abernathy Forest'";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(1, collection.Count);
+            Assert.Equal(4, collection.First().DomainControllers.Count);
+            Assert.All(collection.First().DomainControllers.Select(m => m.Dc), dc => Assert.Null(dc));
+            Assert.Equal("Abernathy Forest", collection.First().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelFilterGtDateNoExpand()
+    {
+        string query = "/forest?$filter=CreatedDate gt 2022-12-26T12:00:00.00Z";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(1, collection.Count);
+            Assert.Equal("Zulauf Forest", collection.First().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelFilterLtDateNoExpand()
+    {
+        string query = "/forest?$filter=CreatedDate lt 2022-12-26T12:00:00.00Z&$orderby=ForestName";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(2, collection.Count);
+            Assert.Equal("Abernathy Forest", collection.First().ForestName);
+            Assert.Equal("Rolfson Forest", collection.Last().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelExpandDcNoFilterAndOrderBy()
+    {
+        string query = "/forest?$top=2&$expand=DomainControllers/Dc&$orderby=ForestName desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(2, collection.Count);
+            Assert.Equal(2, collection.First().DomainControllers.Count);
+            Assert.Equal(4, collection.Last().DomainControllers.Count);
+            Assert.All(collection.First().DomainControllers.Select(m => m.Dc), dc => Assert.NotNull(dc));
+            Assert.All(collection.Last().DomainControllers.Select(m => m.Dc), dc => Assert.NotNull(dc));
+            Assert.Equal("Zulauf Forest", collection.First().ForestName);
+            Assert.Equal("Rolfson Forest", collection.Last().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelNoExpandNoFilterAndOrderBy()
+    {
+        string query = "/forest?$orderby=ForestName desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(3, collection.Count);
+            Assert.Equal(new List<string> { "Zulauf Forest", "Rolfson Forest", "Abernathy Forest" }, collection.Select(m => m.ForestName));
+        }
+    }
+
+    [Fact]
+    public async void ForestModelNoExpandFilterEqAndOrderBy()
+    {
+        string query = "/forest?$top=5&$filter=ForestName eq 'Rolfson Forest'&$orderby=ForestName desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(1, collection.Count);
+            Assert.Equal(4, collection.Single().DomainControllers.Count);
+            Assert.All(collection.Single().DomainControllers.Select(m => m.Dc), dc => Assert.Null(dc));
+            Assert.Equal("Rolfson Forest", collection.First().ForestName);
+        }
+    }
+
+    [Fact]
+    public async void ForestModelExpandDcSelectForestNameAndBackupExpandBackupFilterNeAndOrderBy()
+    {
+        string query = "/forest?$top=5&$select=ForestName&$expand=DomainControllers/Dc($select=FullyQualifiedDomainName,Backups;$expand=Backups($select=ForestId, Location))&$filter=ForestName ne 'Zulauf Forest'&$orderby=ForestName desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        static void Test(ICollection<ForestModel> collection)
+        {
+            Assert.Equal(2, collection.Count);
+            Assert.Equal(new[] { "Rolfson Forest", "Abernathy Forest" }, collection.Select(m => m.ForestName));
+
+            var dcs = collection.SelectMany(m => m.DomainControllers.Select(m => m.Dc)).ToList();
+            Assert.All(dcs.Select(m => m.FullyQualifiedDomainName), fqdn => Assert.NotNull(fqdn));
+            Assert.All(dcs.SelectMany(m => m.Backups), backup => Assert.NotNull(backup));
+
+            var backups = dcs.SelectMany(dc => dc.Backups).ToList();
+            Assert.All(backups.Select(m => m.ForestId), id => Assert.NotEqual(default, id));
+            Assert.All(backups.Select(m => m.Location), location => Assert.NotNull(location));
+            Assert.All(backups.Select(m => m.Location.Credentials), creds => 
+            {
+                Assert.NotNull(creds);
+                Assert.NotNull(creds.Username);
+                Assert.NotNull(creds.Password);
+            });
+            Assert.All(backups.Select(m => m.Location.NetworkInformation), info =>
+            {
+                Assert.NotNull(info);
+                Assert.NotNull(info.Address);                
+            });
+        }
+    }
+
+    [Fact]
+    public async void OpsTenantExpandBuildingsExpandBuilderExpandCityFilterNeAndOrderBy()
+    {
+        string query = "/ForestModel?$top=5&$expand=Buildings($expand=Builder($expand=City))&$filter=Name ne 'One'&$orderby=Name desc";
+        Test(Get<ForestModel, Forest>(query));
+        Test(await GetAsync<ForestModel, Forest>(query));
+        Test(await GetUsingCustomNameSpace<ForestModel, Forest>(query));
+
+        void Test(ICollection<ForestModel> collection)
+        {
+
+        }
+    }
+
 
     [Fact]
     public async Task ForestSelectValues_NestedFilter_ShouldReturnFilteredLiteralCollectionOfValues()
