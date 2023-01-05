7b182040694f264c8a1a64425be738789e652c58
Warren <wbuckley15@hotmail.com>
2023-01-04 19:52:03 -0500

    clean up

---
+++ AutoMapper.AspNetCore.OData.Cosmos/Visitors/MemberFilterAppender.cs
@@ -0,0 +1,80 @@
+﻿using LogicBuilder.Expressions.Utils;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.OData.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AutoMapper.AspNet.OData.Visitors
+{
+    internal sealed class MemberFilterAppender : VisitorBase
+    {
+        private readonly PathSegment collectionSegment;
+
+        private MemberFilterAppender(List<PathSegment> pathSegments, ODataQueryContext context) 
+            : base(pathSegments, context)
+        {
+            this.collectionSegment = this.pathSegments.First(e => e.MemberType.IsList());
+        }
+
+        public static Expression AppendFilters(Expression expression, List<PathSegment> pathSegments, ODataQueryContext context) =>
+            new MemberFilterAppender(pathSegments, context).Visit(expression);
+
+        protected override Expression MatchedExpression(PathSegment pathSegment, MemberInitExpression node, MemberAssignment binding)
+        {
+            if (pathSegment != this.collectionSegment)
+                return base.VisitMemberInit(node);
+
+            return Expression.MemberInit
+            (
+                Expression.New(node.Type),
+                node.Bindings.OfType<MemberAssignment>().Select(UpdateBinding)
+            );
+
+            MemberAssignment UpdateBinding(MemberAssignment assignment)
+            {
+                if (assignment != binding)
+                    return assignment;
+
+                Type segmentType = pathSegment.ElementType;
+
+                ParameterExpression param = Expression.Parameter(segmentType);
+                MemberExpression memberExpression = GetMemberExpression(param);
+
+                return assignment.Update(GetBindingExpression(binding, param, memberExpression));
+            }
+        }
+
+        private MemberExpression GetMemberExpression(ParameterExpression param) =>
+            this.pathSegments.Skip(CurrentIndex() + 1).Aggregate
+            (
+                Expression.MakeMemberAccess(param, this.pathSegments[CurrentIndex()].Member),
+                (expression, next) => Expression.MakeMemberAccess(expression, next.Member)
+            );
+
+        private Expression GetBindingExpression(MemberAssignment binding, ParameterExpression param, MemberExpression memberExpression)
+        {
+            var parameters = new Dictionary<string, ParameterExpression>();
+
+            Type memberType = this.pathSegments.Last().ElementType;
+            FilterHelper helper = new(parameters, this.context);
+
+            LambdaExpression lambdaExpression = helper
+                .GetFilterPart(GetFilter().Expression)
+                .GetFilter(memberType, parameters, helper.LiteralName);
+
+            lambdaExpression = Expression.Lambda
+            (
+                MemberAccessReplacer.Replace(memberType, memberExpression, lambdaExpression.Body),
+                false,
+                param
+            );
+
+            return FilterInserter.Insert(param.Type, lambdaExpression, binding);
+        }
+
+        private FilterClause GetFilter() =>
+            this.pathSegments.Last().FilterOptions.FilterClause;        
+    }
+}
---
+++ AutoMapper.AspNetCore.OData.Cosmos/Visitors/QueryMethodAppender.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper.Execution;
+using LogicBuilder.Expressions.Utils;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.OData.UriParser;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Xml.Linq;
+
+namespace AutoMapper.AspNet.OData.Visitors
+{
+    internal sealed class QueryMethodAppender : ExpressionVisitor
+    {
+        private readonly List<ODataExpansionOptions> expansionPath;
+        private readonly ODataQueryContext context;
+        private int currentIndex;
+
+        public QueryMethodAppender(List<ODataExpansionOptions> expansions, ODataQueryContext context)
+        {
+            this.expansionPath = expansions;
+            this.context = context;
+            this.currentIndex = 0;
+        }
+
+        public static Expression AppendQuery(Expression expression, List<ODataExpansionOptions> expansions, ODataQueryContext context) =>
+            new QueryMethodAppender(expansions, context).Visit(expression);
+
+        protected override Expression VisitMemberInit(MemberInitExpression node)
+        {
+            if (TryGetCurrent(out var expansion))
+            {
+                Type nodeType = node.Type.GetCurrentType();
+                Type parentType = expansion.ParentType.GetCurrentType();
+
+                if (nodeType == parentType && GetBinding(out var binding))
+                {
+                    if (expansion.QueryOptions is not QueryOptions options)
+                    {
+                        Next();
+                        return base.VisitMemberInit(node);
+                    }
+
+                    return Expression.MemberInit
+                    (
+                        Expression.New(node.Type),
+                        node.Bindings.OfType<MemberAssignment>().Select(UpdateBinding)
+                    );
+
+                    MemberAssignment UpdateBinding(MemberAssignment assignment)
+                    {
+                        if (assignment != binding)
+                            return assignment;
+
+                        Next();
+                        return assignment.Expression switch
+                        {
+                            MethodCallExpression expr => assignment.Update(GetCallExpression(expr, expansion)),
+                            _ => assignment.Update(GetBindingExpression(assignment, options))
+                        };
+                    }
+                }
+            }
+            return base.VisitMemberInit(node);
+
+            bool GetBinding([MaybeNullWhen(false)] out MemberAssignment binding)
+            {
+                binding = node.Bindings.OfType<MemberAssignment>()
+                   .FirstOrDefault(b => b.Member.Name.Equals(expansion.MemberName));
+
+                return binding is not null;
+            }
+        }
+
+        private Expression GetCallExpression(MethodCallExpression callExpression, ODataExpansionOptions expansion) =>
+            MethodAppender.AppendQueryMethod(callExpression, expansion, this.context);
+
+        private Expression GetBindingExpression(MemberAssignment memberAssignment, QueryOptions options)
+        {
+            return memberAssignment.Expression;
+            //Type elementType = memberAssignment.Expression.Type.GetCurrentType();
+            //return Expression.Call
+            //(
+            //    LinqMethods.EnumerableWhereMethod.MakeGenericMethod(elementType),
+            //    memberAssignment.Expression,
+            //    clause.GetFilterExpression(elementType, context)
+            //).ToListCall(elementType);
+        }
+
+        private void Next()
+        {
+            if (this.currentIndex < this.expansionPath.Count)
+                ++this.currentIndex;
+
+        }
+
+        private bool TryGetCurrent([MaybeNullWhen(false)] out ODataExpansionOptions options)
+        {
+            options = currentIndex < this.expansionPath.Count
+                ? this.expansionPath[this.currentIndex]
+                : null;
+
+            return options is not null;
+        }
+    }
+}
