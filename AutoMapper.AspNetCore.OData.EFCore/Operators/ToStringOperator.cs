95a3f5125cf24737cfbd7507641c82378576a884
Warren <wbuckley15@hotmail.com>
2023-01-15 13:35:00 -0500

    add folder

---
+++ AutoMapper.AspNetCore.OData.EFCore/Operators/ToStringOperator.cs
@@ -0,0 +1,36 @@
+﻿using LogicBuilder.Expressions.Utils;
+using LogicBuilder.Expressions.Utils.ExpressionBuilder;
+using System;
+using System.Linq.Expressions;
+
+namespace AutoMapper.AspNet.OData.Operators
+{
+    internal sealed class ToStringOperator : IExpressionPart
+    {
+        private readonly IExpressionPart source;
+
+        public ToStringOperator(IExpressionPart source) =>
+            this.source = source;
+
+        public Expression Build() => Build(source.Build());
+
+        private Expression Build(Expression expression) => expression.Type switch
+        {
+            Type type when type.IsNullableType() => ConvertNullable(expression),
+            _ => expression.GetObjectToStringCall()
+        };
+
+        private static Expression ConvertNullable(Expression expression)
+        {
+            Expression memberAccess = expression.MakeValueSelectorAccessIfNullable();
+            return Expression.Condition
+            (
+                expression.MakeHasValueSelector(),
+                memberAccess.GetObjectToStringCall(),
+                Expression.Constant(null, typeof(string))
+            );
+        }
+    }
+}
+
+
